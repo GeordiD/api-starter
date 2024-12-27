f4ee7830f3dfa035b8463829a1d62c25e490ac9d
GeordiD <geordi.dosher@gmail.com>
2024-12-27 09:29:53 -0800

    fadtify quick start

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-const message: string = 'hello world';
+import Fastify from 'fastify';
 
-console.log(message);
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get('/', async () => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
